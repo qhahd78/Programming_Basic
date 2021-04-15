407b1af0023ee73cd5d240024439148c9c5a5698
qhahd78 <umuj0708@likelion.org>
2021-04-15 21:31:02 +0900

    W4 free ing

--- W4/Low.py
+++ W4/Low.py
@@ -15,15 +15,23 @@ scores.reverse()
 
 print(scores)
 
+# 새로운 배열 low_scores 생성
 low_scores = []
+
+# append 로 낮은 점수 3개를 배열에 넣어준다. 
 low_scores.append(str(scores[-1]))
 low_scores.append(str(scores[-2]))
 low_scores.append(str(scores[-3]))
 
+# Low.txt 를 열어준다. (write)
 low_result = open("Low.txt", "w")
 
+# low_scores 를 한 바퀴 돌면서 한 줄에 하나씩 배열의 요소 하나를 출력해준다. 
 for i in low_scores:
+    # 배열의 요소 하나를 출력
     low_result.write(i)
+    # 한 줄씩 출력하기 위해 줄 바꿈 
     low_result.write("\n")
-    
+
+# 파일 닫기 
 low_result.close()
---
+++ W4/free.py
@@ -0,0 +1,10 @@
+
+# words=[]
+result_word = open('freee.txt', 'r', encoding='utf-8')
+
+for line in result_word: 
+    print(line, end='')
+
+text = line
+print(text)
+result_word.close()
