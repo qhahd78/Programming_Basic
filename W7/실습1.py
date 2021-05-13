ae9be768c2501745e716e7a2e33725c07c3e0c64
qhahd78 <umuj0708@likelion.org>
2021-05-13 23:02:08 +0900

    W7 ing

--- W7/실습1.py
+++ W7/실습1.py
@@ -1,14 +1,14 @@
-import pygame.mixer
+import pygame.mixer # mmixer 임포트. 사운드를 메모리에 적재 후 연주한다. 
 
-sounds = pygame.mixer
-sounds.init()
+sounds = pygame.mixer 
+sounds.init() # 사운드 초기화 
 
-def wait_finish(channel):
-    while channel.get_busy():
-        pass
+def wait_finish(channel): # 함수 정의 
+    while channel.get_busy(): # get_busy() 가 거짓일 때까지 반복 
+        pass # 아무일도 하지 않고 넘어간다. 
 
-s = sounds.Sound("heartbeat.wav")
-wait_finish(s.play())
+s = sounds.Sound("heartbeat.wav") # Sound 객체 생성 
+wait_finish(s.play()) # 
 
 s2 = sounds.Sound("buzz.wav")
 wait_finish(s2.play())
