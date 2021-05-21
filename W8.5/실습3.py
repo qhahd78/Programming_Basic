d9f52e3c536dde3cf721f3ba81a0edd721b9a1ef
qhahd78 <umuj0708@likelion.org>
2021-05-21 15:46:49 +0900

    W8.5 Lecture

---
+++ W8.5/실습3.py
@@ -0,0 +1,9 @@
+try:
+    x = float(input("숫자 입력: "))
+    inverse = 1.0 / x 
+except ValueError as e : 
+    print(e)
+except ZeroDivisionError as e:
+    print("0으로 나눈 에러: " + str(e))
+finally:
+    print("예외가 발생하지 않거나 또는 발생할 수 있음.")
